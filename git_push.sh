5072ca0de04c71266e8510eae0e7f7f7dffc206d
GitHub Action <action@github.com>
2021-02-02 17:24:43 +0000

    Order Service commit hash: 8e3a3ea

--- git_push.sh
+++ git_push.sh
@@ -18,7 +18,7 @@ if [ "$git_repo_id" = "" ]; then
 fi
 
 if [ "$release_note" = "" ]; then
-    release_note="Order Service commit hash: e9c1dd3"
+    release_note="Order Service commit hash: 8e3a3ea"
     echo "[INFO] No command line input provided. Set \$release_note to $release_note"
 fi
 
